adff5aa69e76f2ace3c29f842289727875066f01
ChrisJosJC <63082513+ChrisJosJC@users.noreply.github.com>
2023-04-04 14:25:58 -0400

    Testing main file

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(morgan('dev'))
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-// Routes
-app.use(require('./src/routes/index'))
+// // Routes
+// app.use(require('./src/routes/index'))
 
-// Static content
-app.use(express.static(path.join(__dirname, '/src/views')))
+// // Static content
+// app.use(express.static(path.join(__dirname, '/src/views')))
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
