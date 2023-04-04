3199b07c2de4392ccbe97ae7637725639a626887
ChrisJosJC <63082513+ChrisJosJC@users.noreply.github.com>
2023-04-04 00:19:05 -0400

    Fix bug Vercel #1

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 // Routes
-app.use(require('./routes/index'))
+app.use(require('./src/routes/index'))
 
 // Static content
-app.use(express.static(path.join(__dirname, 'views')))
+app.use(express.static(path.join(__dirname, '/src/views')))
 
 app.listen(3000)
 console.log("Starting server...");
